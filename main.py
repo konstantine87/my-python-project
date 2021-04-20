c29f7118dbb04801fc87ad24f09052ed33a1c0cf
Kote <kote8715@gmail.com>
2021-04-20 23:55:44 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class DriveBot:
+    def __init__(self):
+        self.motor_speed = 0
+        self.direction = 0
+        self.sensor_range = 0
+
+    # Add the methods here!
+    def control_bot(new_speed, )
+
+
+robot_1 = DriveBot()
+# Use the methods here!
+
+# print(robot_1.motor_speed)
+# print(robot_1.direction)
+# print(robot_1.sensor_range)
